e470ad1c59f7bb54874289ed061432f5d3fa238c
JaredBLewis <jared.lewis@ucas-edu.net>
2025-01-31 11:39:09 -0700

    Program Python

---
+++ variables/program.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(void){
+    scanf("what is your name:");
+    printf("You are %f years old.\n");
+    return 0;
+}
+
---
+++ variables/program.py
@@ -0,0 +1,7 @@
+# Jared Lewis, First Python Program
+
+name = input("what is your name:")
+
+age = input("How old are you:")
+
+print("Hi", name, "you are", age, "years old. :)")
