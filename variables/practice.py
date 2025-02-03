ca7bef3c524fbaed811580af3d032a25b64b154d
JaredBLewis <jared.lewis@ucas-edu.net>
2025-02-03 11:09:57 -0700

    Python Practice

--- variables/practice.py
+++ variables/practice.py
@@ -1,7 +1,7 @@
 # Jared Lewis, Python Notes.
 name = input("What's your name:")
 small_number = 9
-Large_number = 117
+large_number = 117
 breakfast = input("What did you have for breakfast:")
 favorite_color = input("what is your favorite color:")
 school = input("What school do you go too:")
@@ -9,4 +9,4 @@ year = input("What is the current year:")
 eye_color = input("What color are your eyes:")
 age = input("How old are you:")
 favorite_subject = input("What is your favorite subject in school:")
-print(name, small_number, Large_number, breakfast, favorite_color, school, year, eye_color, age, favorite_subject)
+print(name, small_number, large_number, breakfast, favorite_color, school, year, eye_color, age, favorite_subject)
