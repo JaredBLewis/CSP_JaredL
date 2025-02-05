2c2b80fd834aa82d2b6e0fb3dea1986b7d25b138
JaredBLewis <jared.lewis@ucas-edu.net>
2025-02-05 11:48:43 -0700

    Class Work

--- expressions/notes.c
+++ expressions/notes.c
@@ -1,16 +1,17 @@
 // Jared Lewis, Expression Notes Python
 #include <stdio.h>
 #include <math.h>
-float x = pow(2,2);
+//float x = pow(2,2);
 //float equation = 5*7+11/(5-2);
 int num = 12;
 float pi = 3.1415926535;
+float expression = 5*pow(7,2)/4;
 // + - * /
 int main(void){
-    num = 4;
-    printf("%d\n", num);
+    //num = 4;
+    //printf("%d\n", num);
     //printf("%f\n", equation);
-
-    printf("%.10f", pi);
+    //printf("%f", expression);
+    //printf("%.10f", pi);
     return 0;
 }
