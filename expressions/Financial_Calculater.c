eac6d8550325b9e76d91a78848046f2a561794c1
JaredBLewis <jared.lewis@ucas-edu.net>
2025-02-06 11:48:53 -0700

    Class Homework

---
+++ expressions/Financial_Calculater.c
@@ -0,0 +1,44 @@
+// Jared Lewis, Financial Calculater C
+#include <stdio.h>
+
+
+int main(void){
+// print statement that welcomes my user, and tells them what the program does.
+
+// ask what their income is (variable an input)
+
+// ask what their rent is (variable an input)
+
+// ask what their utilities is (variable an input)
+
+// ask what their groceries is (variable an input)
+
+// ask what their transportation is (variable an input)
+
+// claculate avings as 10% of income (income*.1) (variable)
+
+// claculate spending as income-savings-rent-utilities-groceries-trensportation (variable)
+
+// calculate percent income of rent (rent/income *100) (variable)
+
+// calculate percent income of utilities (utilities/income *100) (variable)
+
+// calculate percent income of groceries (groceries/income *100) (variable)
+
+// calculate percent income of transportation (transportation/income *100) (variable)
+
+// calculate percent income of spending (spending/income *100) (variable)
+
+// your rent is $XX.XX and which is XX% of your income. (print)
+
+// your utilities is $XX.XX and which is XX% of your income. (print)
+
+// your groceries is $XX.XX and which is XX% of your income. (print)
+
+// your transportation is $XX.XX and which is XX% of your income. (print)
+
+// your savings is $XX.XX and which is XX% of your income. (print)
+
+// your spending is $XX.XX and which is XX% of your income. (print)
+    return 0;
+}
---
+++ expressions/Financial_Calculator.py
@@ -0,0 +1,56 @@
+# Jared Lewis, Financial Calculater Python
+
+# print statement that welcomes my user, and tells them what the program does.
+
+# ask what their income is (variable an input)
+income = float(input("How much is your monthly income?:"))
+
+# ask what their rent is (variable an input)
+rent = float(input("How much does your monthly rent?:"))
+
+# ask what their utilities is (variable an input)
+utilities = float(input("How much do your monthly utlities cost?:"))
+
+# ask what their groceries is (variable an input)
+groceries = float(input("How much do your monthly groceries cost?:"))
+
+# ask what their transportation is (variable an input)
+transportation = float(input("How much does your monthly transportation cost?:"))
+
+# claculate avings as 10% of income (income*.1) (variable)
+print()
+
+# claculate spending as income-savings-rent-utilities-groceries-trensportation (variable)
+
+
+# calculate percent income of rent (rent/income *100) (variable)
+
+
+# calculate percent income of utilities (utilities/income *100) (variable)
+
+
+# calculate percent income of groceries (groceries/income *100) (variable)
+
+
+# calculate percent income of transportation (transportation/income *100) (variable)
+
+
+# calculate percent income of spending (spending/income *100) (variable)
+
+
+# your rent is $XX.XX and which is XX% of your income. (print)
+
+
+# your utilities is $XX.XX and which is XX% of your income. (print)
+
+
+# your groceries is $XX.XX and which is XX% of your income. (print)
+
+
+# your transportation is $XX.XX and which is XX% of your income. (print)
+
+
+# your savings is $XX.XX and which is XX% of your income. (print)
+
+
+# your spending is $XX.XX and which is XX% of your income. (print)
