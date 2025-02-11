2cf5a9b04b52373e8947b3a5ae941ec742d74534
JaredBLewis <jared.lewis@ucas-edu.net>
2025-02-11 10:59:11 -0700

    class work

---
+++ strings/notes.py
@@ -0,0 +1,16 @@
+# Jared Lewis, Strings Notes Python
+
+#string is a data type that holds any info surrounded by qoutation marks  "" ''
+
+#print("Hello World")
+#note ="Hello World"
+note ='Hello World'
+
+#name = input("What is your first name?:\n").srtip().lower().capitalize()
+
+#print(f"hello {name} wlcome to my program!")
+
+print(note)
+
+sentance = "The quick brown fox jumps over the lazy dog"
+
