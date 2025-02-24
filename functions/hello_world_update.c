1bfc86103a5a4359e778d3b99e0846a52370675f
JaredBLewis <jared.lewis@ucas-edu.net>
2025-02-24 07:31:57 -0700

    VS code

--- functions/hello_world_update.c
+++ functions/hello_world_update.c
@@ -1,2 +1,15 @@
 // Jared Lewis, Hello World Update C
 #include <stdio.h>
+
+void greeting(char name[]){
+    return (printf("Hello, %s!\n", name));
+}
+
+int main(void){
+    greeting("Jared");
+    greeting("Jack");
+    greeting("Bob");
+    greeting("Alice");
+    greeting("Yenesis");
+    return 0;
+}
