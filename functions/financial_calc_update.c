a4c3d5668bced8872eae06acee0ef2361523bc27
JaredBLewis <jared.lewis@ucas-edu.net>
2025-02-26 11:50:59 -0700

    Class Work

--- functions/financial_calc_update.c
+++ functions/financial_calc_update.c
@@ -1,2 +1,56 @@
 // Jared Lewis, Financial Calculator Update C
 #include <stdio.h>
+#include <math.h>
+float income, rent, utilities, groceries, transportation, savings, spending;
+
+void percent(char type[],int amount){
+    int per = amount/ income *100;
+    printf("The percent of your income for %s is %d%%.\n", type, per);
+}
+
+float user_input(char type[]){
+    return (user_input("How much is your monthly %s?: ", type));
+
+}
+
+int main(void){
+    // print statement that welcomes my user, and tells them what the program does.
+    printf("\n");
+    printf("Hi there! wlecome to my program!\n");
+    printf("This program will calculate the best way to spend your money.\n");
+    // ask what their income is (variable an input)
+    income = user_input("income");
+    // ask what their rent is (variable an input)
+    printf("How much does your monthly rent cost?:");
+    scanf("%d", &rent);
+
+    // ask what their utilities is (variable an input)
+    printf("How much does your monthly utilities cost?:");
+    scanf("%d", &utilities);
+
+    // ask what their groceries is (variable an input)
+    printf("How much does your monthly groceries cost?:");
+    scanf("%d", &groceries);
+
+    // ask what their transportation is (variable an input)
+    printf("How much does your monthly transportation cost?:");
+    scanf("%d", &transportation);
+
+    // calculate percent of income for each category
+    float savings = income * 0.1;
+    float spending = (float)income - savings - rent - utilities - groceries - transportation;
+    float per_rent = (float)rent / income * 100;
+    float per_utilities = (float)utilities / income * 100;
+    float per_groceries = (float)groceries / income * 100;
+    float per_transportation = (float)transportation / income * 100;
+    float per_spending = (float)spending / income * 100;
+
+    // give
+    percent("rent", rent);
+    percent("uttilities", utilities);
+    percent("groceries", groceries);
+    percent("transportation", transportation);
+    percent("savings", savings);
+    percent("spending", spending);
+    return 0;
+}
--- functions/financial_calc_update.py
+++ functions/financial_calc_update.py
@@ -1,30 +1,23 @@
 # Jared Lewis, Financial Calculator Update Python
 
-def input(type):
-    return print(input(f"How much is your monthly {type}?:"))
+def user_input(type):
+    return float(input(f"How much is your monthly {type}?: "))
 
 def info(cost, income, type):
     percent = cost/income *100
     print(f"Your {type} is ${cost:.2f} which is {percent}% of your income.")
 
-def w
 # print statement that welcomes my user, and tells them what the program does.
-print("Hi there, and welcome to my program!\n")
-print("This program will calculate the best way to spend your money.\n")
-# ask what their income is (variable an input)
-income = input("What is your monthly income?:")
+print("\n")
+print("Hi there, and welcome to my program!")
+print("This program will calculate the best way to spend your money.")
 
-# ask what their rent is (variable an input)
-rent = float(input("How much does your monthly rent cost?:"))
-
-# ask what their utilities is (variable an input)
-utilities = float(input("How much do your monthly utlities cost?:"))
-
-# ask what their groceries is (variable an input)
-groceries = float(input("How much do your monthly groceries cost?:"))
-
-# ask what their transportation is (variable an input)
-transportation = float(input("How much does your monthly transportation cost?:"))
+# collect user inputs
+income = user_input("income")
+rent = user_input("rent")
+utilities = user_input("utilities")
+groceries = user_input("groceries")
+transportation = user_input("transportation")
 
 # claculate savings as 10% of income (income*.1) (variable)
 savings = income*.1
