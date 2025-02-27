30279d04beda801cab7bb96c182ca1e9d05c8a28
JaredBLewis <jared.lewis@ucas-edu.net>
2025-02-27 19:18:19 +0000

    Class Work

--- conditionals/time_of_day.c
+++ conditionals/time_of_day.c
@@ -3,6 +3,18 @@
 #include <time.h>
 
 int main(void){
-    printf("Hello World!\n");
+    printf("\n");
+    printf("Hello, and welcome to my program!\n");
+    char name[20];
+    printf("What is your name?: ");
+    scanf("%s", name);
+
+    time_t rawtime;
+    struct tm * timeinfo;
+    timeinfo = localtime(&rawtime);
+    time_t now = time(NULL);
+    struct tm * tm_struct = localtime(&now);
+    int hour = tm_struct->tm_hour;
+    printf("%d\n", hour);
     return 0;
 }
