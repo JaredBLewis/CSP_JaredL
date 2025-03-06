cda41cd012c1d81083aa296db7f0f002341818ef
JaredBLewis <jared.lewis@ucas-edu.net>
2025-03-06 11:27:42 -0700

    Class Work

--- loops/fizzbuzz.c
+++ loops/fizzbuzz.c
@@ -1,7 +1,20 @@
 // Jared Lewis, FizzBuzz C
 #include <stdio.h>
 
+int x = 1;
+
 int main(void){
-    printf("Hello World!\n");
+    while(x<=50){
+        if(x%3==0 && x%5==0){
+            printf("FizzBuzz\n");
+        }else if(x%5==0){
+            printf("Buzz\n");
+        }else if(x%3==0){
+            printf("Fizz\n");
+        }else{
+            printf("%d\n", x);
+        }
+        x += 1;
+    }
     return 0;
 }
--- loops/fizzbuzz.py
+++ loops/fizzbuzz.py
@@ -1,1 +1,15 @@
 # Jared Lewis, FizzBuzz Python
+
+x = 1
+
+while x < 51:
+    
+    if x % 3==0 and x % 5==0:
+        print("FizzBuzz")
+    elif x % 5==0:
+        print("Buzz")
+    elif x % 3==0:
+        print("Fizz")
+    else:
+        print(x)
+    x += 1
