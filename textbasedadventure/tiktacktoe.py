1714b30c3623fa74b363d4f6cca74663d6ab2fcc
JaredBLewis <jared.lewis@ucas-edu.net>
2025-03-21 11:48:25 -0600

    ticktacktoe

---
+++ textbasedadventure/tiktacktoe.py
@@ -0,0 +1,11 @@
+# Jared Lewis, Tick Tack Toe Python
+
+import random
+
+print("\n")
+print("Welcome to Tick Tack Toe!")
+print("You are 0 and the computer is X.")
+print("[ 1 | 2 | 3 ]")
+print("[ 4 | 5 | 6 ]")
+print("[ 7 | 8 | 9 ]")
+print("Please enter the number of the square you want to play in: \n")
