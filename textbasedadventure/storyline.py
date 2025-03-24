bdb26dfedf573540cfe6adf0e9a2e13e7cce84dd
JaredBLewis <jared.lewis@ucas-edu.net>
2025-03-24 18:18:01 +0000

    Stuff

--- textbasedadventure/storyline.py
+++ textbasedadventure/storyline.py
@@ -1,21 +1,24 @@
 # Jared Lewis, Storyline
 import time
 
-delay = 0.01
+# sets the delay between each character printed
+delay = 0.035
 
-message = "Welcome user...\n\
-The game you are about to play is a text based adventure game.\n\
-This game will test your skills through a serios of minigames.\n\
-May I welcome you to...\n\
-\n\
-THE UNDERWORLD\n\
-created by:\n\
-Alan De Lara\n\
-Anthony Peterson\n\
-Douglas London\n\
-Jared Lewis\n\
-"
+message = "Welcome user...\n"\
+"The game you are about to play is a text based adventure game.\n"\
+"This game will test your skills through a series of minigames.\n"\
+"May I welcome you to...\n"\
+"\n"\
+"THE UNDERWORLD\n"\
+"created by:\n"\
+"Alan De Lara\n"\
+"Anthony Peterson\n"\
+"Douglas London\n"\
+"Jared Lewis\n"\
+"\n"\
+"Please Press Enter to Continue"
 
+# function to print the message with a delay.
 for char in message:
     print(char, end="")
     time.sleep(delay)
