72733c5159bfdf6c2807982e806c9f717199a5e3
JaredBLewis <jared.lewis@ucas-edu.net>
2025-04-22 11:49:05 -0600

    Class

---
+++ javascript/notes.js
@@ -0,0 +1,25 @@
+let images = ["https://images.pexels.com/photos/46148/aircraft-jet-landing-cloud-46148.jpeg", "https://grist.org/wp-content/uploads/2015/12/plane.jpg?quality=75&strip=all", "https://airadvisor.com/Media/images/news/big/334d699ae1a52e2134f005124360ccfd.webp"]
+let counter = 0
+
+function change(){
+    if (counter < images.length){
+        document.getElementById("img").src = images[counter]
+        counter +=1
+    }else{
+        counter = 0
+        document.getElementById ("img").src = images [counter]
+    }
+}
+
+function hello(){
+    document.getElementById("title").innerHTML = "Hello World!"
+}
+function hover(){
+    document.getElementById("img").src = "https://grist.org/wp-content/uploads/2015/12/plane.jpg?quality=75&strip=all"
+}
+function leave(){
+    document.getElementById("img").src = "https://images.pexels.com/photos/46148/aircraft-jet-landing-cloud-46148.jpeg"
+}
+function hidden(){
+    document.getElementById("meme").style.display = "block"
+}
