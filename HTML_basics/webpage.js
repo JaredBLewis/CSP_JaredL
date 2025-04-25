2990f86a29d5a2382e712ea708a27b09413bf654
JaredBLewis <jared.lewis@ucas-edu.net>
2025-04-24 22:51:41 -0600

    Finished First Webpage

--- HTML_basics/webpage.js
+++ HTML_basics/webpage.js
@@ -1,9 +1,15 @@
-function hover(){
-    document.getElementById("img").src = "https://grist.org/wp-content/uploads/2015/12/plane.jpg?quality=75&strip=all"
+function change() {
+    const img = document.getElementById("img");
+    img.src = "https://static.vecteezy.com/system/resources/thumbnails/047/269/681/small_2x/thumb-up-emoji-isolated-in-transparent-background-png.png";
+    document.getElementById("imgsrc").innerHTML = "Image From: Vectreezy";
 }
-function leave(){
-    document.getElementById("img").src = "https://images.pexels.com/photos/46148/aircraft-jet-landing-cloud-46148.jpeg"
+
+function ResetImage() {
+    const img = document.getElementById("img");
+    img.src = "https://media.cnn.com/api/v1/images/stellar/prod/210910234902-01-vaccine-protest-ca-0829.jpg?q=w_3000,h_2000,x_0,y_0,c_fill";
+    document.getElementById("imgsrc").innerHTML = "Image From: CNN";
 }
+
 function view(){
     if(document.getElementById("paragraph").style.display === "block"){
         document.getElementById("paragraph").style.display = "none"
@@ -12,4 +18,10 @@ function view(){
     document.getElementById("paragraph").style.display = "block"
     document.getElementById("btn").innerHTML = "Show Less"
     }
+}
+function scrollToBottom() {
+    window.scrollTo({
+        top: document.body.scrollHeight,
+        behavior: "smooth"
+    });
 }
