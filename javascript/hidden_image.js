90ee5618c936723af35076982f7405265ea6ce7b
JaredBLewis <jared.lewis@ucas-edu.net>
2025-04-25 00:00:19 -0600

    Almost done with hidden image

---
+++ javascript/hidden_image.js
@@ -0,0 +1,9 @@
+function view() {
+    var img = document.getElementById("hidden_image");
+    img.style.visibility = "visible";
+}
+
+function hide() {
+    var img = document.getElementById("hidden_image");
+    img.style.visibility = "hidden";
+}
