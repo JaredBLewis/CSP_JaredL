184b8a7bf4ed4aeae6c7a7f83ad666f65825e4a0
JaredBLewis <jared.lewis@ucas-edu.net>
2025-05-05 11:49:11 -0600

    class work

--- Final/staycation_project.js
+++ Final/staycation_project.js
@@ -1,8 +1,8 @@
 function show(){
-    if(document.getElementById("hidden").style.display === "block"){
+    if(document.getElementById("hidden").style.display === "flex"){
         document.getElementById("hidden").style.display = "none"
         document.getElementById("button").innerHTML = "Show More"
     }else{
-    document.getElementById("hidden").style.display = "block"
+    document.getElementById("hidden").style.display = "flex"
     document.getElementById("button").innerHTML = "Show Less"}
 }
